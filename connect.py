e9a006994b693c5269090443ccabfe3bbdef039d
Felix Otoo <ofelix03@gmail.com>
2020-02-22 23:22:33 +0000

    [IMP]

--- connect.py
+++ connect.py
@@ -1,14 +1,17 @@
 import pysftp as sftp
 
 
-def connect(host='64.227.51.202', port=22, username='root', private_key='./id_rsa'):
-    cnopts = sftp.CnOpts()
-    cnopts.hostkeys = None
-    cnopts.log = False
-    cnopts.hostkeys = None
+def connect(host='64.227.51.202', port=22, username='root', private_key='./id_rsa', default_path='.'):
+    # cnopts = sftp.CnOpts()
+    # cnopts.hostkeys = None
+    # cnopts.log = False
+    # cnopts.hostkeys = None
     server = sftp.Connection(port=port,
                              host=host,
                              username=username,
-                             private_key=private_key)
+                             private_key=private_key,
+                             # default_path=default_path,
+                             # cnopts=cnopts
+                             )
 
     return server
